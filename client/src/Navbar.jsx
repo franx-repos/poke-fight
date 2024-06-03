84a376099ccfdaf88973a9d23dae8a9f00d72881
Dsnirl Jim <Jens.Daniel.Klein@gmail.com>
2024-06-03 12:43:40 +0200

    Navbar added

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -1,17 +1,55 @@
+import React from "react";
+import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
 import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/">Arena</NavLink>
-        </li>
-        <li>
-          <NavLink to="leaderboard">Leaderboard</NavLink>
-        </li>
-      </ul>
-    </nav>
+    <AppBar sx={{ backgroundColor: "rgba(0, 0, 0, 1)" }}>
+      <Toolbar>
+        <Typography
+          className="headline"
+          variant="h4"
+          component="div"
+          padding="1rem"
+          fontFamily="'Pokemon Solid', sans-serif"
+          letterSpacing=".2rem"
+          color="#FFCD09"
+          sx={{
+            flexGrow: 1,
+            position: "relative",
+            left: "50%",
+            transform: "translateX(-50%)",
+            mt: -2,
+          }}>
+          PokeFight
+        </Typography>
+        <Box sx={{ display: "flex" }}>
+          <Button
+            component={NavLink}
+            to="/"
+            sx={{
+              color: "white",
+              textDecoration: "none",
+              position: "relativ",
+              right: "90%",
+            }}
+            exact>
+            Arena
+          </Button>
+          <Button
+            component={NavLink}
+            to="/leaderboard"
+            sx={{
+              color: "white",
+              textDecoration: "none",
+              position: "relative",
+              right: "25%",
+            }}>
+            Leaderboard
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 }
 
