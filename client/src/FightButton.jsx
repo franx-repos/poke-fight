29ce5244ce24bb7b841dbb1c7b0925b176e5fa89
Frank Jäkel-Försterling <jaekel.frank@gmx.de>
2024-06-03 16:24:07 +0200

    more

--- client/src/FightButton.jsx
+++ client/src/FightButton.jsx
@@ -102,12 +102,12 @@ function FightButton({ pokeId1, pokeId2, pokemons, setWinner }) {
   return (
     <div>
       <button onClick={handleFight} disabled={isFighting}>
-        Kampf Starten
+        Fight
       </button>
       {fightResult.length > 0 && (
         <div>
-          <h1 className="highlight">Kampfprotokoll:</h1>
-          <ul>
+          <h1 className="highlight">Battle course:</h1>
+          <ul className="highlight">
             {fightResult.map((entry, index) => (
               <li key={index}>{entry}</li>
             ))}
@@ -119,59 +119,3 @@ function FightButton({ pokeId1, pokeId2, pokemons, setWinner }) {
 }
 
 export default FightButton;
-
-// import { useState } from "react";
-
-// function FightButton({ pokeId1, pokeId2, pokemons, setWinner }) {
-//   const [fightResult, setFightResult] = useState(null);
-
-//   const handleFight = () => {
-//     const poke1 = pokemons.find((poke) => poke.id === pokeId1);
-//     const poke2 = pokemons.find((poke) => poke.id === pokeId2);
-
-//     if (!poke1 || !poke2) {
-//       console.error("Fehler: Pokémon-Daten sind nicht verfügbar.");
-//       return;
-//     }
-
-//     const poke1Strength =
-//       poke1.base.Attack +
-//       poke1.base.Defense +
-//       poke1.base["Sp. Attack"] +
-//       poke1.base["Sp. Defense"] +
-//       poke1.base.Speed;
-
-//     const poke2Strength =
-//       poke2.base.Attack +
-//       poke2.base.Defense +
-//       poke2.base["Sp. Attack"] +
-//       poke2.base["Sp. Defense"] +
-//       poke2.base.Speed;
-
-//     let winnerName;
-
-//     if (poke1Strength > poke2Strength) {
-//       winnerName = poke1.name.english;
-//     } else if (poke1Strength < poke2Strength) {
-//       winnerName = poke2.name.english;
-//     } else {
-//       winnerName = "Unentschieden";
-//     }
-
-//     setWinner(winnerName);
-//     setFightResult(`Der Gewinner ist: ${winnerName}`);
-//   };
-
-//   return (
-//     <div>
-//       <button onClick={handleFight}>Kampf Starten</button>
-//       {fightResult && (
-//         <div>
-//           <h1 className="highlight">{fightResult}</h1>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default FightButton;
