b064b681f366a22462aaf16ee82617fb55cdc033
Saeedmora <hlipor77@gmail.com>
2024-06-07 13:39:39 +0200

    die letzte veränderung mongodb

--- server/server.js
+++ server/server.js
@@ -1,30 +1,23 @@
 import express from "express";
 import pokemons from "./data.js";
 import cors from "cors";
+import connectDB from "./db.js";
+import dotenv from "dotenv";
+import pokemonRoutes from "./routes/pokemon.js"; // Import der Routen
 
+dotenv.config();
+
+connectDB();
 const app = express();
 const PORT = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Routen für feste Pokémon-Daten
 app.get("/pokemons", (req, res) => res.json(pokemons));
-//   .get((req, res) => res.send("We retrieve a ressource."))
-//   .post((req, res) => res.send("We create a ressource."))
-//   .put((req, res) => res.send("We update a ressource."))
-//   .patch((req, res) => res.send("We update a ressource partially."))
-//   .delete((req, res) => res.send("We delete a ressource."));
-// app.get("/something", (req, res) => res.status(418).send("Kram"));
 
-app.get("/pokemons/:id", (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  const pokemon = pokemons.find((pokemon) => pokemon.id === id);
+// Routen für dynamische Daten (XP, Wins, Losses)
+app.use("/api/pokemon", pokemonRoutes);
 
-  if (pokemon) {
-    res.json(pokemon);
-  } else {
-    res.status(404).send({ error: "Pokémon not found" });
-  }
-});
-
-app.listen(PORT, () => console.log("Sever is running on PORT: " + PORT));
+app.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
